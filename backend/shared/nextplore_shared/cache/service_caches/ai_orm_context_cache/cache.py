849105b20840e3365551111e7bfdcb4f3f9e45a8
OleynikovNikolay <nikolay.oleynikov96@gmail.com>
2025-08-11 12:43:08 +0200

    add cache functionality to ai_orm_context_service_cache

--- backend/shared/nextplore_shared/cache/service_caches/ai_orm_context_cache/cache.py
+++ backend/shared/nextplore_shared/cache/service_caches/ai_orm_context_cache/cache.py
@@ -1,5 +1,8 @@
 from nextplore_shared.contracts.ai_orm_context_service.avilable_models_response import AvailableModelsResponse
-from nextplore_shared.cache.utils.key_factory import get_string_cache_key
+from nextplore_shared.contracts.ai_orm_context_service.orm_context_request import ORMContextRequest
+from nextplore_shared.contracts.ai_orm_context_service.orm_context_response import ORMContextResponse
+from nextplore_shared.cache.utils.key_factory import get_string_cache_key, get_cache_key
+from nextplore_shared.identity_service.identity_model.user_identity import UserIdentity
 from nextplore_shared.cache.client.base_redis_client import BaseCache
 
 
@@ -8,7 +11,7 @@ class AIORMContextServiceCache(BaseCache):
         super().__init__(namespace='ai_orm_context_cache_service', version='v1')
 
     async def get_models(self) -> AvailableModelsResponse:
-        cache_key = get_string_cache_key(value='available_models', prefix='models')
+        cache_key = get_string_cache_key(value='available-models', prefix='models')
         return await self.get_one(
             cache_key,
             model=AvailableModelsResponse
@@ -19,11 +22,40 @@ class AIORMContextServiceCache(BaseCache):
         response: AvailableModelsResponse,
         ttl: int = 600
     ) -> None:
-        cache_key = get_string_cache_key(value='available_models', prefix='models')
+        cache_key = get_string_cache_key(value='available-models', prefix='models')
         await self.set_one(
             cache_key,
             value=response,
             ttl=ttl
         )
+
+    async def get_orm_context(
+        self, 
+        user_identity: UserIdentity,
+        request: ORMContextRequest
+    ) -> ORMContextResponse:
+        
+        cache_key = get_cache_key(model=request, prefix='orm-context')
+        return await self.get_one(
+            user_identity.organization_id,
+            user_identity.user_id,
+            cache_key,
+            model=ORMContextResponse
+        )
+    
+    async def set_orm_context(
+        self, 
+        user_identity: UserIdentity,
+        request: ORMContextRequest,
+        response: ORMContextResponse
+    ) -> None:
+        cache_key = get_cache_key(model=request, prefix='orm-context')
+        await self.set_one(
+            user_identity.organization_id,
+            user_identity.user_id,
+            cache_key,
+            value=response
+        )
+        
     
 ai_orm_context_service_cache = AIORMContextServiceCache()
