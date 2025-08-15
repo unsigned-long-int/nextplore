9ff5edd69a151c56d8148daad933928e22f052a2
OleynikovNikolay <nikolay.oleynikov96@gmail.com>
2025-08-15 12:47:01 +0200

    add logging for schema registry

--- backend/messaging/schema_registry_client/confluent.py
+++ backend/messaging/schema_registry_client/confluent.py
@@ -1,9 +1,11 @@
 import json
-
+import logging
 from typing import Dict, Callable
 from confluent_kafka.schema_registry import SchemaRegistryClient
 from confluent_kafka.schema_registry.schema_registry_client import Schema
 
+
+logger = logging.getLogger(__name__)
 
 class ConfluentSchemaRegistryClient:
     def __init__(self, registry_url: str, schema_dispatcher: Callable[[str], str]) -> None:
@@ -16,6 +18,7 @@ class ConfluentSchemaRegistryClient:
         schema_str = self._schema_dispatcher(topic)
 
         if subject in self._cache:
+            logger.info(f'Cache hit for subject: {subject}')
             return self._cache[subject]
         
         try:
@@ -24,11 +27,15 @@ class ConfluentSchemaRegistryClient:
             if json.loads(latest_schema_str) == json.loads(schema_str):
                 self._cache[subject] = latest_schema.schema_id
                 return latest_schema.schema_id
+            logger.info(f'New schema discovered: updating...')
             schema_id = self._registry.register_schema(subject, Schema(schema_str, 'AVRO'))
+            logger.info(f'Schema registered: {schema_id}. Subject: {subject}')
             self._cache[subject] = schema_id
             return schema_id
         except Exception:
+            logger.info(f'Schema not found: registering...')
             schema_id = self._registry.register_schema(subject, Schema(schema_str, 'AVRO'))
+            logger.info(f'Schema registered: {schema_id}. Subject: {subject}')
             self._cache[subject] = schema_id
             return schema_id
 
